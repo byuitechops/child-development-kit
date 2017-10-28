e5114db22fdd2ff3bb61a632f3ce9e85e8364cf0
Zachary Williams <wil11063@byui.edu>
2017-10-27 18:18:09 -0600

    Initial framework

---
+++ updateCanvasGauntlets.js
@@ -0,0 +1,12 @@
+/* Dependences */
+const cleaner = require('canvas-course-repentance');
+const updateLocal = require('./updateD2LGauntlets.js');
+
+
+// Run canvas repentance on gauntlet courses
+
+// Run preImport dev environment up-to-date
+
+// Run conversion process on now up-to-date local gauntlet D2L Zips
+
+// Upload into Canvas Gauntlets
---
+++ updateD2LGauntlets.js
@@ -0,0 +1,31 @@
+/* Dependencies */
+const downloader = require('d2l-course-downloader');
+const decompress = require('decompress');
+const del = require('del');
+const chalk = require('chalk');
+const asyncLib = require('async');
+
+/* Deletes any old gauntlet zips or files */
+del(['./D2LOriginal/*', './D2LProcessing/*']).then(paths => {
+    if (paths.length > 0) {
+        console.log(chalk.yellow('Outdated Zips/Files deleted:\n', paths.join('\n')));
+    } else {
+        console.log(chalk.yellowBright('No outdated files to be deleted.\n'));
+    }
+    /* Download all of the gauntlet courses */
+    downloader(courses => {
+        asyncLib.each(courses, (course, callback) => {
+            /* Unzip the gauntlet courses */
+            decompress(course.filePath, `./D2LProcessing/${course.name}`)
+            .then((files) => {
+                console.log(chalk.blueBright(`${course.name} successfully unzipped`));
+                callback(null);
+            }, (promiseError) => {
+                if (promiseError) console.error(promiseError);
+                callback(null);
+            });
+        }, error => {
+            console.log(chalk.cyanBright(`Your local Gauntlet Test Courses are now up-to-date.`));
+        });
+    });
+});
