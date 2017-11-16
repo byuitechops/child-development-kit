f28c8afe6daa5dba72000069e3cd9faa3ba067bd
Zachary Williams <wil11063@byui.edu>
2017-11-16 12:59:27 -0700

    Fix

--- updateCanvasGauntlets.js
+++ updateCanvasGauntlets.js
@@ -1,6 +1,6 @@
 /* Dependences */
 const updateLocal = require('./updateD2LGauntlets.js');
-const conversion = require('./node_modules/d2l-to-canvas-conversion-tool/gauntletCLI.js');
+const conversion = require('./node_modules/d2l-to-canvas-conversion-tool/gauntletCli.js');
 
 module.exports = () => {
     conversion(resultCourses => {
