7011a0b8e2e860db0d168de0ed67a556119dd3cb
Zachary Williams <wil11063@byui.edu>
2017-11-30 14:33:26 -0700

    Fixes

--- updateD2LGauntlets.js
+++ updateD2LGauntlets.js
@@ -1,34 +1,32 @@
 /* Dependencies */
-const downloader = require('d2l-course-downloader').gauntletDownload;
+const downloader = require('d2l-course-downloader');
 const decompress = require('decompress');
 const del = require('del');
 const chalk = require('chalk');
-const asyncLib = require('async');
+const path = require('path');
 
 module.exports = () => {
     /* Deletes any old gauntlet zips or files */
-    del(['./node_modules/child-development-kit/D2LOriginal/*', './node_modules/child-development-kit/D2LProcessing/*']).then(paths => {
+    del(['./node_modules/child-development-kit/D2LOriginal/*',
+        './node_modules/child-development-kit/D2LProcessing/*'
+    ]).then(paths => {
         if (paths.length > 0) {
             console.log(chalk.yellow('Outdated Zips/Files deleted:\n', paths.join('\n')));
         } else {
             console.log(chalk.yellowBright('No outdated files to be deleted.\n'));
         }
+
+        /* Download Information */
+        var downloadData = {
+            ous: ['340002', '340007', '340008', '340009'],
+            downloadLocation: path.resolve('.', 'node_modules/child-development-kit/D2LOriginal')
+        };
+
         /* Download all of the gauntlet courses */
-        downloader(courses => {
-            asyncLib.each(courses, (course, callback) => {
-                /* Unzip the gauntlet courses */
-                decompress(course.filePath, `./node_modules/child-development-kit/D2LProcessing/${course.name}`)
-                .then((files) => {
-                    console.log(chalk.blueBright(`${course.name} successfully unzipped`));
-                    callback(null);
-                }, (promiseError) => {
-                    if (promiseError) console.error(promiseError);
-                    callback(null);
-                });
-            }, error => {
-                if (error) console.error(error);
-                console.log(chalk.cyanBright(`Your local Gauntlet Test Courses are now up-to-date.`));
-            });
+        downloader(downloadData, (error, courses) => {
+            if (error) console.error(error);
+            console.log(chalk.cyanBright(
+                `Your local Gauntlet Test Courses are now up-to-date.`));
         });
     });
 }
