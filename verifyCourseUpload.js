6e830434012d99b02acee2235719c5a4c24d62c0
Zachary Williams <wil11063@byui.edu>
2018-01-22 13:02:53 -0700

    Add verify code

--- verifyCourseUpload.js
+++ verifyCourseUpload.js
@@ -2,12 +2,11 @@ const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
-    course.addModuleReport('verify-course-upload');
 
     /* Options for our "retry" function */
     var options = {
         times: 15,
-        interval: 2000
+        interval: 5000
     };
 
     var manifest = course.content.find(file => {
@@ -21,15 +20,15 @@ module.exports = (course, stepCallback) => {
         /* Get modules from course */
         canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules`, (err, modules) => {
             if (err) {
-                course.throwErr('verify-course-upload', err);
+                course.error(err);
                 callback(err);
             } else {
                 /* If we get back an empty array or not */
                 if (modules.length != moduleCount) {
-                    course.success('verify-course-upload', `Course has finished unpacking.`);
+                    course.message(`Course has finished unpacking.`);
                     callback(null, modules);
                 } else {
-                    course.success('verify-course-upload', `Course has not finished unpacking. Checking again.`);
+                    course.message(`Course has not finished unpacking. Checking again.`);
                     callback(new Error('Blank array received'));
                 }
             }
