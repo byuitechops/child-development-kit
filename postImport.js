b4ed68b17bf657efcadf279393539174766351bf
Zachary Williams <wil11063@byui.edu>
2018-03-07 15:36:38 -0700

    Working iteration (I think)

---
+++ postImport.js
@@ -0,0 +1,60 @@
+const canvas = require('canvas-wrapper');
+const copyCourse = require('copy-a-canvas-course');
+const chalk = require('chalk');
+const { childType } = require('../../package.json');
+
+module.exports = (course, callback) => {
+
+    function getPristine() {
+        return new Promise((resolve, reject) => {
+            var gauntletNum = course.info.fileName.split('.zip')[0];
+            gauntletNum = gauntletNum[gauntletNum.length - 1];
+            canvas.get(`/api/v1/accounts/1/courses?search_term=${gauntletNum} (Pristine)`, (getErr, foundCourse) => {
+                if (getErr) return reject(getErr);
+                if (course.length < 1) return reject(new Error('Cannot find Pristine Gauntlet.'));
+                resolve(foundCourse[0]);
+            });
+        });
+    }
+
+    function copyTheCourse(foundCourse) {
+        return new Promise((resolve, reject) => {
+            copyCourse(foundCourse.id, 19, (err, newCourse) => {
+                if (err) return reject(err);
+                course.info.canvasOU = newCourse.id;
+                resolve(newCourse);
+            });
+        });
+    }
+
+    function changeName(newCourse) {
+        return new Promise((resolve, reject) => {
+            var today = new Date();
+            var minutes = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes();
+            canvas.put(`/api/v1/courses/${newCourse.id}`,
+                {
+                    'course[name]': `Conversion Gauntlet ${today.getMonth() + 1}/${today.getDate()} ${today.getHours()}:${minutes} - ${course.info.author}`,
+                    'course[course_code]': `CG ${today.getMonth() + 1}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`,
+                },
+                (err, changedCourse) => {
+                    if (err) return reject(err);
+                    console.log(chalk.blueBright(`Gauntlet Course Name: ${chalk.greenBright(changedCourse.id)} - Conversion Gauntlet ${today.getMonth() + 1}/${today.getDate()} ${today.getHours()}:${minutes}`));
+                    resolve(changedCourse)
+                });
+        });
+    }
+
+    if (childType === 'preImport') {
+        return course;
+    }
+
+    return getPristine()
+        .then(copyTheCourse)
+        .then(changeName)
+        .then(changedCourse => {
+            callback(null, course);
+        })
+        .catch(callback);
+}
+
+
--- runModule.js
+++ runModule.js
@@ -1,16 +1,40 @@
-const updater = require('./gauntlets.js');
+const updater = require('./updater.js');
 const asyncLib = require('async');
+const { author } = require('../../package.json')
+
+var gauntlets = [
+    '340002',
+    '340007',
+    '340008',
+    '340009',
+];
+
+var gauntletNum = () => {
+    return (isNaN(+process.argv[3]) ? 0 : (process.argv[3] - 1));
+};
+
+var courseData = {
+    domain: 'byui',
+    platform: 'online',
+    D2LOU: gauntlets[gauntletNum()],
+    cleanUpModules: [],
+    preImportModules: [],
+    postImportModules: [],
+    name: `Conversion Test Gauntlet ${gauntletNum() + 1}`,
+    author: author
+};
 
 function runChildModule() {
     return new Promise((resolve, reject) => {
         asyncLib.waterfall([
+            asyncLib.constant(courseData),
             require('create-course-object'),
             require('./adjustFilePaths.js'),
-            require('index-directory'),
-            // require('./postImport.js'),
+            require('index-directory').conversionTool,
+            require('./postImport.js'),
             require('../../main.js')
         ], (err, course) => {
-            // if (err) return reject(err);
+            if (err) return reject(err);
             resolve(course);
         });
     });
@@ -18,4 +42,7 @@ function runChildModule() {
 
 updater()
     .then(runChildModule)
+    .then((course) => {
+        console.log(`Process complete! Course Link:\n https://byui.instructure.com/courses/${course.info.canvasOU}`);
+    })
     .catch(console.error);
