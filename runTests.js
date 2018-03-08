70563604588ce09fad2db021c941bd8fe7bfd681
Zachary Williams <wil11063@byui.edu>
2018-03-08 09:36:57 -0700

    Test for tests

---
+++ runTests.js
@@ -0,0 +1,23 @@
+// // const tap = require('tap');
+// const tests = require('../../Tests/childTests.js');
+// const asyncLib = require('async');
+// const { spawn } = require('child_process');
+
+// module.exports = (course) => {
+
+//     // Get all of the different modules' tests and run them
+
+//     console.log(tests);
+
+//     // asyncLib.series(tests, err => {
+//     //     if (err) console.log(err);
+//     //     else {
+//     //         console.log('Tests complete');
+//     //     }
+//     // });
+
+// };
+
+// var tester = spawn('ls').exec();
+
+
