15f1df9090e15b8dac30e9d612333210b36dcb3b
Zachary Williams <wil11063@byui.edu>
2018-04-24 09:08:29 -0600

    Emphasize npm test path change error

--- testChild.js
+++ testChild.js
@@ -1,1 +1,3 @@
-console.log('Please change "npm test" in your package.json to: \n "tap ./node_modules/child-development-kit/runChild.js -T"');
+const chalk = require('chalk');
+
+console.log(chalk.redBright('Please change "npm test" in your package.json to: \n "tap ./node_modules/child-development-kit/runChild.js -T"'));
